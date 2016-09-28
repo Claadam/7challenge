4631abc6edc8652585485d56c1c508c8b6c8dd26
WalnutsAreCool <claadam@nuevaschool.org>
2016-09-28 08:44:09 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-console.log("")
+console.log(":/")
 var colorfulElements = $(".intro-section")
 var colorNameDisplay = $("#color-display")
+console.log(":|")
 
 colorfulElements.hover(function(e) {
   console.log(":D")
   var elementHoveredOver = $(e.target)
-  colorNameDisplay.html("");
+  colorNameDisplay.html("Green is not a creative color");
 })

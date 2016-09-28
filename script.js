75a82f00b34843a078f4cdf9878b15abf13e9461
WalnutsAreCool <claadam@nuevaschool.org>
2016-09-28 08:42:41 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+console.log("")
 var colorfulElements = $(".intro-section")
 var colorNameDisplay = $("#color-display")
 
 colorfulElements.hover(function(e) {
+  console.log(":D")
   var elementHoveredOver = $(e.target)
   colorNameDisplay.html("");
 })
